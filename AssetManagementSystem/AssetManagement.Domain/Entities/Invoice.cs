7e88845e82c638786d44b0379ddce7d52b0d8e63
Nhlaka <nhlakambotho@gmail.com>
2016-06-28 13:53:23 -0700

    Added Invoice  Upload functionality. Altered the following : 1) Database 2) Context Class 3) New Domain Class 4) Add Method in Stock Controller 5) Altered View

---
+++ AssetManagementSystem/AssetManagement.Domain/Entities/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Domain.Entities
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        [StringLength(100)]
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
